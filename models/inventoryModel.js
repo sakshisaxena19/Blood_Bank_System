b7ab50886920eea377be72de0340b79c60f2c337
sakshisaxena19 <sakshidimpi0@gmail.com>
2024-07-04 15:26:30 +0530

    testing

---
+++ models/inventoryModel.js
@@ -0,0 +1,40 @@
+let mongoose = require("mongoose");
+let inventorySchema = new mongoose.Schema(
+  {
+    inventoryType: {
+      type: String,
+      required: [true, "inventory type is required*"],
+      enum: ["in", "out"],
+    },
+    bloodGroup: {
+      type: String,
+      required: [true, "blood group is required *"],
+      enum: ["O+", "O-", "AB+", "AB-", "A+", "A-", "B+", "B-"],
+    },
+    quantity: {
+      type: String,
+      required: [true, "blood quanity is required *"],
+    },
+    organization: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "users",
+      required: [true, "Organization is required *"],
+    },
+    hospital: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "users",
+      required: function () {
+        return this.inventoryType === "out";
+      },
+    },
+    donar: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "users",
+      required: function () {
+        return this.inventoryType === "in";
+      },
+    },
+  },
+  { timestamps: true }
+);
+module.exports = mongoose.model("inventory", inventorySchema);
